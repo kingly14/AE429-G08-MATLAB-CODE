0a16e894d7ad1e58806827b5d3072224ccb8e03a
Ryan <38165380+MyUsernameIsGood@users.noreply.github.com>
2024-02-06 15:11:22 -0500

    fixed code

--- ISA_APP.m
+++ ISA_APP.m
@@ -15,7 +15,7 @@ fprintf('Density            (Kg/m^3): %f\n', density)
 fprintf('Relative Density   (Kg/m^3): %f\n', density / 1.2250)
 
 
-%% OTHER EQUATIONS
+%% OTHER EQUATIONS USED FOR VERIFICATION
 
 a = .00356616; % F/ft
 
@@ -25,7 +25,7 @@ P0 = 0;
 p0 = 0;
 h0 = 0;
 
-T = T0 - a(geopotentialAltitude-h0);
+T = T0 - a * (geopotentialAltitude-h0);
 Theta = 1-(6.875E-6)*geopotentialAltitude;
 P = P0 * Theta^5.2561;
 p = p0 * Theta^4.2561;
