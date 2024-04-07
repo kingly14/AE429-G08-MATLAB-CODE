fdad9566796c7b8c3d824712193bb59179869e39
Ryan <kingrm@clarkson.edu>
2024-04-07 14:00:15 -0400

    Fixed L/D

--- Cruise Performance/validation.m
+++ Cruise Performance/validation.m
@@ -13,7 +13,7 @@ me = mi - 141610; % AIRCRAFT EMPTY WEIGHT
 g = 9.8;
 
 % Geometric Properties
-p = 0.22;       % Density               [kg/m^3]
+p = 0.20;       % Density               [kg/m^3]
 S = 371.6122;   % Planform Area         [m^2]
 b = 56.4;       % Wing Span             [m]
 oswald = 0.9;   % Oswald Efficiency
@@ -21,9 +21,9 @@ AR = b^2/S;     % Aspect Ratio
 k = 1 / (pi * oswald * AR);
 
 % Aerodyaanmic Properties
-CD0 = 0.006;    % Coeficient Zero Drag XFLR5 Analysis
+CD0 = 0.01;    % Coeficient Zero Drag XFLR5 Analysis
 CL = 0.462;     % Lift Coeficient form XFLR5 Analysis
-CD = CD0 + (k*CL^2);
+CD = CD0 + k*(CL^2);
 
 %Powerplant Properties
 fj = 2.2e-5 * 8;
@@ -33,7 +33,7 @@ V = 290;
 
 %% Calculate Forces
 W = mi * g;
-L = W;
+L = 1/2 * p * V^2 * S * CL;
 D = 1/2 * p * V^2 * S * CD;
 T = D;
 
@@ -52,6 +52,7 @@ fprintf('Percent Error: %f\n', percentError)
 
 % Endurance+++
 
-% maxLD = 1/(sqrt(4*CD0*k))
-% 
-% V_MAX_RC = velocityAtMaxRateOfClimb((W/S), p, CD0, (D/W), maxLD)
+maxLD = 1/(sqrt(4*CD0*k))
+liftToDrag = L/D
+
+endurance = maxEnduranceJet(fj, (L/D), mi, me) / 3600
