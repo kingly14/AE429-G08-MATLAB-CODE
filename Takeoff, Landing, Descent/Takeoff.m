1ecbfdab203fdf2602281e819e2c3533c1b7d4dc
uhlts <uhlts@clarkson.edu>
2024-04-22 14:37:06 -0400

    Fixed Takeoff

--- Takeoff, Landing, Descent/Takeoff.m
+++ Takeoff, Landing, Descent/Takeoff.m
@@ -2,15 +2,17 @@ clear
 clc
 
 % Mass Properties
-mi = 219600; % MAX AIRCRAFT TAKEOFF MASS kg
-me = mi - 141610; % AIRCRAFT EMPTY WEIGHT kf
-g = 9.81;
+mi = 219600/0.4536; % MAX AIRCRAFT TAKEOFF MASS lb
+mis = 219600; % kg
+me = (mi/.4536) - (141610/.4536); % AIRCRAFT EMPTY WEIGHT kf
+g = 32.2;
 
 % Geometric Properties
-p = 0.20;       % Density               [kg/m^3]
-S = 371.6122;   % Planform Area         [m^2]
+p = 0.002377;       % Density               [slugs/ft^3]
+S = 4000;   % Planform Area         [ft^2]
+Ss = 3702; %[m^2]
 
-b = 56.4;       % Wing Span             [m]
+b = 185;       % Wing Span             [ft]
 e = 0.9;   % Oswald Efficiency
 AR = b^2/S;   % Aspect Ratio
 k = 1 / (pi * e * AR);
@@ -20,41 +22,43 @@ k = 1 / (pi * e * AR);
 CD0 = 0.024;   
 CL = 0.637;
 CD = CD0 + k*(CL^2);
-Clmax = 1.6;
+Clmax = 0.9;
 
 %Powerplant Properties
 fj = 0.0000269;
 
 % Cruise Properties
-V = 290;
+V = 951.444;
 
 %% Calculate Forces
-W = mi * g; %N
+W = 488000; %lb
+Ws = mis*9.81; %N
 L = 1/2 * p * V^2 * S * CL;
 D = 1/2 * p * V^2 * S * CD;
-T = D;
-k1 = 427029.27506; %N
-k2 = 25; %N*s/m
-k3 = 1.11E-2; %N*s/m^2
+T = 96000;
+k1 = 96000; %lb
+k2 = 30.28; %lb*ft/m
+k3 = 1.317E-2; %lb*s/ft^2
 liftToDrag = L/D;
-h = 12; %Height of wings above ground [m]
-mu = 0.4;
+h = 37.3701; % Height of wings above ground [ft]
+mu = 0.04;
 N = 3;
 Kuc = 4.5E-5;
 hOB = 35;
 
 vStall = sqrt((2/p)*(W/S)*(1/Clmax))
-vLO = 1.11*vStall;
-v = 0.7*vLO;
-Tr = k1 -k2*v+k3*v^2; %Thrust
-kT = ((Tr/W)-mu);
-deltaCD0 = (W/S)*Kuc*(mi^-0.215);
-G = (((16*h)/b^2)/(1+(16*h)/b^2));
-kA = ((-p)/2*(W/S))*(CD0+deltaCD0+((k1+(G/(pi*e*AR)))*(CL^2)-mu*CL));
-sg = (1/(2*g*kA))*log(1+(kA/kT)*vLO^2)+N*vLO
-
-R = (6.96*vStall^2)/g
-theta = acosd(1-(hOB/7510))
-sa = R*sind(theta)
+% vLO = 1.11*vStall;
+% v = 0.7*vLO;
+% Tr = k1 -(k2*v)+(k3*v^2); %Thrust
+% kT = ((Tr/W)-mu);
+% deltaCD0 = (Ws/Ss)*Kuc*(mis^-0.215);
+% G = ((16*h)/b)^2/(1+(16*h)/b)^2;
+% kA = -(p/(2*(W/S)))*(CD0+deltaCD0+((k+(G/(pi*e*AR)))*CL^2)-mu*CL);
+% sg = (1/(2*g*kA))*log(1+((kA/kT)*vLO^2))+(N*vLO)
+ 
+sg = (1.21*(W/S))/(g*p*Clmax*(T/W));
+R = (6.96*vStall^2)/g;
+theta = acosd(1-(hOB/7510));
+sa = R*sind(theta);
 
 s = sa + sg
